a73953cb0bad0c243e9ebbf88f89cf9d84c6839b
AntoninSevera <115144138+AntoninSevera@users.noreply.github.com>
2022-11-24 11:38:19 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
-basic.forever(function () {
-	
+let number = 0;
+//input.onButtonPressed(Button.A, function() {
+//    if (number > 0)
+//   {
+//       number -= 1
+//    }
+//    whaleysans.showNumber(10)
+//})
+//input.onButtonPressed(Button.B, function() {
+//    if (number < 10) {
+//        number += 1
+//    }
+//    whaleysans.showNumber(0)
+//})
+basic.showNumber(number);
+basic.forever(function() {
+    let isPressed = input.buttonIsPressed(Button.A);
+    if (isPressed) {
+        if (number < 50) {
+            number += 1
+        }
+    }
+    let bPressed = input.buttonIsPressed(Button.B);
+    if (bPressed) {
+        if (number > 0) {
+            number -= 1
+        }
+    }
+    whaleysans.showNumber(number)
 })
