f9c26df7da0a40c85a9e7a7ae79349037d815ee1
AntoninSevera <115144138+AntoninSevera@users.noreply.github.com>
2022-12-08 11:12:58 +0100

    3. hodina

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-let number = 0;
+//let number = 0;
 //input.onButtonPressed(Button.A, function() {
 //    if (number > 0)
 //   {
@@ -12,19 +12,63 @@ let number = 0;
 //    }
 //    whaleysans.showNumber(0)
 //})
-basic.showNumber(number);
-basic.forever(function() {
-    let isPressed = input.buttonIsPressed(Button.A);
-    if (isPressed) {
-        if (number < 50) {
-            number += 1
-        }
+//basic.showNumber(number);
+//basic.forever(function() {
+//    let isPressed = input.buttonIsPressed(Button.A);
+ //   if (isPressed) {
+  //      if (number < 50) {
+//            number += 1
+  //      basic.pause(200)
+    //    }
+    //}
+    //let bPressed = input.buttonIsPressed(Button.B);
+//    if (bPressed) {
+  //      if (number > 0) {
+    //        number -= 1
+      //  basic.pause(200)
+        //}
+  //  }
+  //  whaleysans.showNumber(number)
+//})
+let number1 = 4
+let previewStateAPressed = input.buttonIsPressed(Button.A);
+let previewStateBPressed = input.buttonIsPressed(Button.B);
+
+let makej2A = () => {
+    number1 -= 1
+    whaleysans.showNumber(number1)
+}
+let makej2B = () => {
+    number1 += 1
+    whaleysans.showNumber(number1)
+}
+
+let makej = function() {
+    if (input.buttonIsPressed(Button.A)) {
+        //let actualStateA = true;
+        //if (previewStateAPressed == false && actualStateA == true)
+        if (!previewStateAPressed) {
+            number1 -= 1
+            previewStateAPressed = true;
+        } 
+    } else {
+        previewStateAPressed = false;
     }
-    let bPressed = input.buttonIsPressed(Button.B);
-    if (bPressed) {
-        if (number > 0) {
-            number -= 1
+
+    if (input.buttonIsPressed(Button.B)) {
+        if (!previewStateBPressed) {
+            number1 += 1
+            previewStateBPressed = true;
         }
+    } else {
+        previewStateBPressed = false
     }
-    whaleysans.showNumber(number)
-})
+    whaleysans.showNumber(number1)
+}
+
+input.onButtonPressed(Button.A, makej2A); //registrace k události...
+input.onButtonPressed(Button.B, makej2B); //registrace k události...
+
+//while (true) {
+//    makej()
+//}
